ddad5d8448b6ae8d4f7661373b3a25ccdb8dba5a
Paul Miller <paul@paulmillr.com>
2012-06-29 15:50:20 +0300

    Add node.js 0.8 & 0.9 support, precompile for npm.

--- test/plugin_test.js
+++ test/plugin_test.js
@@ -6,11 +6,11 @@ describe('Plugin', function() {
   });
 
   it('should be an object', function() {
-    expect(plugin).to.be.ok();
+    expect(plugin).to.be.ok;
   });
 
   it('should has #compile method', function() {
-    expect(plugin.compile).to.be.a(Function);
+    expect(plugin.compile).to.be.an.instanceof(Function);
   });
 
   it('should compile and produce valid result', function(done) {
@@ -18,7 +18,7 @@ describe('Plugin', function() {
     var expected = 'var a = 6;';
 
     plugin.compile(content, 'file.js', function(error, data) {
-      expect(error).not.to.be.ok();
+      expect(error).not.to.be.ok;
       expect(data).to.equal(expected)
       done();
     });
